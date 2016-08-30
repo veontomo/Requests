505b98171cde922775e6826eb1937909406d799a
Andrey Shcherbakov <veontomo@gmail.com>
2016-08-30 16:52:57 +0200

    feat: stub a class that configurates the server

--- src/com/veontomo/Main.java
+++ src/com/veontomo/Main.java
@@ -1,27 +1,40 @@
 package com.veontomo;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
+
+import org.json.simple.JSONObject;
+
 class Main {
 
     public static void main(String[] args) throws InterruptedException {
+        if (args.length == 0){
+            System.out.println("Please, provide the hostname as the input argument");
+        }
+
+        System.out.println("Hostname is " + args[0]);
         final String[] clickPool = new String[]{
-                "http://192.168.5.95/news/venditori/venditori",
-                "http://192.168.5.95/news/venditori/rappresentanti",
-                "http://192.168.5.95/news/no-such-route/no",
-                "http://192.168.5.95/news/rappresentanti/start?annuncio=00000001819",
-                "http://192.168.5.95/news/images/venditori/TRACK-QUERY/logo_natale_2015.jpg"
+                "/news/venditori/venditori",
+                "/news/venditori/rappresentanti",
+                "/news/no-such-route/no",
+                "/news/rappresentanti/start?annuncio=00000001819",
+                "/news/images/venditori/TRACK-QUERY/logo_natale_2015.jpg"
         };
 //        final String[] viewPool = new String[]{
 //                "http://192.168.5.95/news/images/venditori/logo_natale_2015.jpg",
 //        };
 
+
         final int size = clickPool.length;
-        final int ACTIONS_PER_WORKER = 5;
-        final int WORKER_NUM = 1000;
-        final int SLOT_NUM = 20;
+        final int ACTIONS_PER_WORKER = 2;
+        final int WORKER_NUM = 20;
+        final int SLOT_NUM = 10;
         int actionsHttp = 0;
         ArrayList<Action> actions;
-
         Counter c = new Counter(SLOT_NUM);
         for (int i = 0; i < WORKER_NUM; i++) {
             actions = new ArrayList<>();
@@ -33,9 +46,35 @@ class Main {
             Worker v = new Worker(actions);
             c.enqueue(v);
         }
+
+
+        JSONObject routes = new JSONObject();
+        routes.put("venditori/venditori", "http://www.venditori.it");
+        routes.put("venditori/offerta", "http://www.venditori.it/fiera_degli_agenti_di_commercio.php");
+        String url = "http://192.168.5.95/news/routes/add";
+        try {
+            HttpURLConnection connection;
+            connection = (HttpURLConnection) new URL(url).openConnection();
+            connection.setDoOutput(true);
+            connection.setRequestMethod("PUT");
+            connection.setRequestProperty("Accept-Charset", BrowserData.encodings[0]);
+            connection.addRequestProperty("User-Agent", BrowserData.userAgents[0]);
+            connection.setRequestProperty("Content-Type", "application/json");
+            OutputStream out = connection.getOutputStream();
+            out.write(routes.toString().getBytes("UTF-8"));
+
+
+            out.close();
+            connection.getInputStream();
+            int responseCode = connection.getResponseCode();
+
+        } catch (IOException ex) {
+            System.out.println("Attention: exception when connecting to " + url + ": " + ex.getMessage());
+            ex.printStackTrace();
+        }
         System.out.println("Number of the actions: " + actionsHttp);
         System.out.println("Starting the game.");
-        c.start();
+//        c.start();
     }
 
 
---
+++ src/com/veontomo/ServerConfigurator.kt
@@ -0,0 +1,11 @@
+package com.veontomo
+
+/**
+ * Manage the server.
+ *
+ * The class contains methods that change the server settings.
+ *
+ */
+class ServerConfigurator(val host: String){
+
+}
